b26ec54e43f70f6daea002d33466aca6a2fc56f5
Vishnu Srinivasan <vishnunaga_srinivasan@gmail.com>
2018-09-30 22:48:43 -0500

    Made the airplane crashing code better

--- assignment1/hw1.py
+++ assignment1/hw1.py
@@ -12,6 +12,8 @@ import pandas as pd
 
 def histogram_times(filename):
 
+    out = []
+    
     flight_data = pd.read_csv(filename)
     flight_data1 = flight_data.dropna(subset = ['Time'])
     flight_data1['Time'] = flight_data1['Time'].str.split(':').str[0]
@@ -33,35 +35,13 @@ def histogram_times(filename):
 
     #Counter for the value 
     flight_data3 = flight_data2.value_counts(normalize=False, sort=True, ascending=False, bins=None, dropna=True)
-    flight_data3.sort_values(ascending = True)
-
-    output = flight_data3.tolist()
-
-    out = [flight_data3[0]]
-    out.append(flight_data3[1])
-    out.append(flight_data3[2])
-    out.append(flight_data3[3])
-    out.append(flight_data3[4])
-    out.append(flight_data3[5])
-    out.append(flight_data3[6])
-    out.append(flight_data3[7])
-    out.append(flight_data3[8])
-    out.append(flight_data3[9])
-    out.append(flight_data3[10])
-    out.append(flight_data3[11])
-    out.append(flight_data3[12])
-    out.append(flight_data3[13])
-    out.append(flight_data3[14])
-    out.append(flight_data3[15])
-    out.append(flight_data3[16])
-    out.append(flight_data3[17])
-    out.append(flight_data3[18])
-    out.append(flight_data3[19])
-    out.append(flight_data3[20])
-    out.append(flight_data3[21])
-    out.append(flight_data3[22])
-    out.append(flight_data3[23])
+    
+    for x in range (len(flight_data3)):
+        out.append(flight_data3[x])
+   
     return out
+
+
 
 
 def weigh_pokemons(filename, weight):
